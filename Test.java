56927bbbbd6ef9f9bf114aeae8bf8c868fbca168
Sufoxed <sufoxed@hotmail.fr>
2015-09-29 19:26:56 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("Ça marche pour moi !"); //Kemy
+		System.out.println("Ã‡a marche pour moi !"); //Kemy
+		System.out.println("Uniquement sur le site bande d'incapable !!") // Sufoxed
 	}
 }
